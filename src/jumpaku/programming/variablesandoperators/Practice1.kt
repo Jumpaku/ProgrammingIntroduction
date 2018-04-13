6b2159dfce7a9bbeb5873e02d2cb7ad8ea37f1da
Jumpaku <itzimeru@gmail.com>
2018-04-13 11:32:30 +0900

    wrote texts

---
+++ src/jumpaku/programming/variablesandoperators/Practice1.kt
@@ -0,0 +1,8 @@
+package jumpaku.programming.variablesandoperators
+
+fun main(args: Array<String>) {
+    val name = "MPC-Man"
+    val mass = 51.7
+    val height = 172.8
+    println("name: $name, mass: $mass, height: $height")
+}
